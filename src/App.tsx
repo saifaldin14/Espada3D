0b7d75e5512956c3a5f9ed09e2182dd7ee72a46b
Saif Al-Din Ali <thesaifaldin@gmail.com>
2024-08-29 21:00:29 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,34 @@ import React from 'react';
 import Canvas3D from './components/Canvas3D';
 import Sidebar from './components/Sidebar';
 import Toolbar from './components/Toolbar';
+import ModelEditor from './components/ModelEditor';
 import { Provider } from 'react-redux';
 import store from './store';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={styles.container}>
         <Sidebar />
-        <div style={{ flex: 1 }}>
+        <div style={styles.mainContent}>
           <Toolbar />
           <Canvas3D />
         </div>
+        <ModelEditor /> {/* Add this here or within the Sidebar */}
       </div>
     </Provider>
   );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+  },
+  mainContent: {
+    flex: 1,
+    display: 'flex',
+  },
 };
 
 export default App;
